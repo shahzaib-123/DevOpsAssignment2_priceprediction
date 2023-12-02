5fdb1ff0900be1eac46de9a9d370d190fd203bf7
Shahzaib Asghar <79242717+shahzaib-123@users.noreply.github.com>
2023-12-02 21:12:33 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,4 +9,4 @@ def client():
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Welcome to the House Price Predictor' in response.data
+    assert b'Predict House Price' in response.data
